6aa88b5a70f3bb1ca7d14eed2e39b3423307536a
Nick Moore <shiny.nickel@gmail.com>
2021-03-28 21:49:50 +0100

    Update test image path and do not close file
    The file needs to remain open to be read, so using
    
      with open ... as image:
        return image
    
    was clearly not the right choice!

--- server/image_camera.py
+++ server/image_camera.py
@@ -6,7 +6,7 @@ import proto.Messages_pb2 as Msg
 class ImageCamera:
   def __init__(self):
     self.logger = logging.getLogger(self.__class__.__name__)
-    self.path = "test/image.jpg"
+    self.path = "test/data/rpi-logo.jpg"
 
   def load_config(self, config: Msg.ConfigV1) -> None:
     if config.path:
@@ -15,7 +15,7 @@ class ImageCamera:
   def take_photo(self) -> BinaryIO:
     self.logger.info("Capturing image %s", self.path)
     try:
-      with open(self.path, mode='rb') as image:
-        return image
+      image = open(self.path, mode='rb')
+      return image
     except IOError as ioe:
       self.logger.error("Failed to capture image", exc_info=ioe)
