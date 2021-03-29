3e8d44c60a8cc661e26461a9dfc87b792b1bae1c
Nick Moore <shiny.nickel@gmail.com>
2021-03-29 21:47:15 +0100

    Start work on database schemas
    Test simply checks that database code creation executes without error
    and the database file exists after doing so.

--- server/database.py
+++ server/database.py
@@ -5,18 +5,24 @@ from typing import BinaryIO
 
 
 class SqliteDatabase:
-  def __init__(self, path: str = "test.sqlite"):
+  def __init__(self, path: str = "rpi_camera_server.sqlite"):
     self.logger = logging.getLogger(self.__class__.__name__)
     self.path = path
     if not os.path.isfile(self.path):
-      conn = self._get_connection()
-      conn.execute('''CREATE TABLE capture (time TIMESTAMP, image BLOB)''')
-      conn.close()
+      self._create_database()
+
+  def _create_database(self):
+    conn = self._get_connection()
+    conn.execute('''CREATE TABLE capture (id INTEGER PRIMARY KEY, time TIMESTAMP, image BLOB)''')
+    conn.execute('''CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT)''')
+    conn.execute('''CREATE TABLE accesses (user_id INTEGER, capture_id INTEGER, status INTEGER,
+                    FOREIGN KEY(user_id) REFERENCES user(id), FOREIGN KEY(capture_id) REFERENCES capture(id))''')
+    conn.close()
 
   def _get_connection(self):
     return sqlite3.connect(self.path, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
 
-  def store_capture(self, image: BinaryIO, timestamp: float):
+  def store_capture(self, image_data: bytes, timestamp: float):
     conn = self._get_connection()
-    conn.execute('''INSERT INTO capture VALUES(?, ?)''', (timestamp, image.read()))
+    conn.execute('''INSERT INTO capture VALUES(?, ?)''', (timestamp, image_data))
     conn.close()
---
+++ server/test/test_database.py
@@ -0,0 +1,12 @@
+from database import SqliteDatabase
+import os
+
+database_path = 'test.sqlite'
+
+
+def test__create_database():
+  if os.path.isfile(database_path):
+    os.remove(database_path)
+  assert not os.path.isfile(database_path)
+  database = SqliteDatabase(path=database_path)
+  assert os.path.isfile(database_path)
